887aa0e583c85a9ad4b72e394cee9f7994be04b1
Maxwell Rodriguez <89595611+iammaxrodriguez@users.noreply.github.com>
2022-03-21 18:30:52 -0400

    Create passengers.c
    helper file containing passenger names, and numbers associated.

---
+++ AirlineSeatingReservation/passengers.c
@@ -0,0 +1,69 @@
+/*
+Maxwell Rodriguez 
+02/27/22
+Source code passengers list file I use in main file for Airline Reservation System. 
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+// simple array that holds 25 people
+char passenger[25][30] = {
+ "BillyBob",
+ "BraxtonMartoano",
+ "ConnorRyan",
+ "DavidLaid",
+ "EthanStaubus",
+ "ErenJeager",
+ "EugeneRodriguez",
+ "GunnerBurkhart",
+ "HarryOsborn",
+ "JakeWarren",
+ "JerryWest",
+ "JoeDirt",
+ "JohnDoe",
+ "JonSkywalker",
+ "KanyeWest",
+ "KyleMcwhirtir",
+ "Lebronjames",
+ "LukeChorba",
+ "MikasaAckerman",
+ "PatrickBateman",
+ "PeterParker",
+ "RyanGosling",
+ "ShellyStone",
+ "TonyRodriguez",
+ "ZaraClop"
+    };
+
+// number array that matches the people, helps me store them 
+int passengerNumber[25] = {
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+};
