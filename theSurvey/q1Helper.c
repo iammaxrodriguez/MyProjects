04789bbaeff857b52f523ff3ffb4249b129db1ef
Maxwell Rodriguez <89595611+iammaxrodriguez@users.noreply.github.com>
2022-03-21 18:28:31 -0400

    Create q1Helper.c
    helper file for survey

---
+++ theSurvey/q1Helper.c
@@ -0,0 +1,22 @@
+/*
+Maxwell Rodriguez 
+03/06/22
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+    // 7 issues to rate on a 1-10 scale. 
+    char *topics[7] = {
+        "Racism",
+        "Climate Change",
+        "College Affordability",
+        "Terrorism",
+        "Drug Addiction",
+        "Job Opportunities",
+        "Illegal Immigration"
+        };
+    int data[7][10];
